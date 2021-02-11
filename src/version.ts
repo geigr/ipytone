c40b3ae20407a069ac2708775bec9947ddcd7889
Benoit Bovy <benbovy@gmail.com>
2021-02-11 11:41:19 +0100

    use eslint and prettier

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Benoit Bovy
 // Distributed under the terms of the Modified BSD License.
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const data = require('../package.json');
 
 /**
