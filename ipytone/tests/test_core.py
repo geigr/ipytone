13d7a47a2e9e4511d4f8705884246ff182536ae2
Benoit Bovy <benbovy@gmail.com>
2021-02-22 23:19:00 +0100

    add Python tests

--- ipytone/tests/test_core.py
+++ ipytone/tests/test_core.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 from ipytone.core import Destination, InternalAudioNode, InternalNode, Param, get_destination
@@ -17,6 +19,37 @@ def test_internal_audio_node():
     assert node.number_of_inputs == 1
     assert node.number_of_outputs == 1
     assert repr(node) == "InternalAudioNode(tone_class='test')"
+
+
+def test_param():
+    param = Param()
+
+    assert param.value == 1
+    assert param.units == "number"
+    assert param.convert is True
+    assert param.min_value == -math.inf
+    assert param.max_value == math.inf
+    assert param.overridden is False
+
+    param2 = Param(min_value=-0.2, max_value=0.2)
+
+    assert param2.min_value == -0.2
+    assert param2.max_value == 0.2
+
+
+@pytest.mark.parametrize(
+    "units,expected_range",
+    [
+        ("audio_range", (-1, 1)),
+        ("normal_range", (0, 1)),
+        ("time", (0, math.inf)),
+        ("decibels", (-math.inf, math.inf))
+    ]
+)
+def test_param_min_max_value(units, expected_range):
+    param = Param(units=units)
+    actual_range = (param.min_value, param.max_value)
+    assert actual_range == expected_range
 
 
 def test_destination():
