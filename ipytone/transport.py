06fe69668175c154297b5ea61f7ecf23d9a44ef0
David Brochart <david.brochart@gmail.com>
2021-03-13 20:24:56 +0100

    Add transport.clear(eventID)

--- ipytone/transport.py
+++ ipytone/transport.py
@@ -1,7 +1,7 @@
 import contextlib
 
 from ipywidgets import Widget, widget_serialization
-from traitlets import Bool, Enum, Instance, List, Unicode
+from traitlets import Bool, Enum, Instance, Int, List, Unicode
 
 from .base import ToneWidgetBase
 
@@ -18,10 +18,13 @@ class Transport(ToneWidgetBase):
     _methods = List(Unicode()).tag(sync=True)
     _packed_args = List(Unicode()).tag(sync=True)
     _toggle_schedule = Bool().tag(sync=True)
+    _toggle_clear = Bool().tag(sync=True)
 
     _interval = Unicode().tag(sync=True)
     _start_time = Unicode().tag(sync=True)
     _duration = Unicode(allow_none=True).tag(sync=True)
+    _py_event_id = Int().tag(sync=True).tag(sync=True)
+    _clear_event_id = Int().tag(sync=True)
 
     state = Enum(["started", "stopped"], allow_none=False, default_value="stopped").tag(sync=True)
 
@@ -32,6 +35,7 @@ class Transport(ToneWidgetBase):
 
     def __init__(self, **kwargs):
         self._is_scheduling = False
+        self._all_event_id = []
         super(Transport, self).__init__(**kwargs)
 
     @contextlib.contextmanager
@@ -40,13 +44,23 @@ class Transport(ToneWidgetBase):
         self._audio_nodes = []
         self._methods = []
         self._packed_args = []
-        yield self
+        yield self._py_event_id
         self._schedule_op = "scheduleRepeat"
         self._interval = interval
         self._start_time = start_time
         self._duration = duration
         self._toggle_schedule = not self._toggle_schedule
         self._is_scheduling = False
+        self._all_event_id.append(self._py_event_id)
+        self._py_event_id = self._py_event_id + 1
+
+    def clear(self, event_id):
+        if event_id not in self._all_event_id:
+            raise RuntimeError(f"Scheduled event ID not found: {event_id}")
+        self._clear_event_id = event_id
+        self._toggle_clear = not self._toggle_clear
+        id_idx = self._all_event_id.index(event_id)
+        del self._all_event_id[id_idx]
 
     def start(self):
         self.state = "started"
